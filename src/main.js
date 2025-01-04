64cd48f15d0ad79bdcec1d8fbed6745c1d202030
yigoro <seba.bertolotti@gmail.com>
2025-01-04 20:17:05 -0300

    solucionado el incoveniente para eliminar productos de la lista

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ Buscador.addEventListener("input", async (e) => {
     Productos.innerHTML = "";
 
     dataFiltrada.forEach(el => {
-      creadoraDeCards(el.id, el.rubro, el.link, el.descripcion, el.agregado, el.comprado);
+      creadoraDeCards(el.id, el.rubro, el.link, el.descripcion, productosAcomprar);
     });
     
 
@@ -49,11 +49,9 @@ document.addEventListener("DOMContentLoaded", async ()=>{
         const [id, rubro, link, descripcion, agregado, comprado] = row.split(",");
         return { id, rubro, link, descripcion, agregado, comprado };
       });
-
-    //console.log(products)
-
+      
     products.map(el => {
-      creadoraDeCards(el.id, el.rubro, el.link, el.descripcion, el.agregado, el.comprado)
+      creadoraDeCards(el.id, el.rubro, el.link, el.descripcion, productosAcomprar)
   })
 
   } catch (error) {
@@ -63,12 +61,11 @@ document.addEventListener("DOMContentLoaded", async ()=>{
 
 });
   
-const creadoraDeCards = (id, rubro, link, descripcion, agregado, comprado) => {
+const creadoraDeCards = (id, rubro, link, descripcion, productosAcomprar) => {
 
   const container = document.createElement("div")
   container.classList.add("producto")
 
-    //const rubro = document.createElement("h3")
     const img = document.createElement("img")
     const desc = document.createElement("p")
     const boton = document.createElement("button")
@@ -80,27 +77,37 @@ const creadoraDeCards = (id, rubro, link, descripcion, agregado, comprado) => {
     desc.innerText = descripcion
     boton.innerText = "Agregar"
 
+      // Verificar si el producto ya está en la lista de productosAcomprar
+      const productoEnLista = productosAcomprar.find(item => item.id === id);
+      if (productoEnLista) {
+        boton.style.backgroundColor = 'grey';
+        boton.innerText = 'Sacar';
+      }
 
     boton.addEventListener("click", ()=>{
       let titulo = ""
       if (boton.innerText == "Agregar") {
         productosAcomprar.push({id,descripcion}) //Agregamos a la lista
+        console.log("agregado id:" + id)  
+
 
         boton.style.backgroundColor = 'grey';
         boton.innerText = "Sacar"
         titulo = "Agregado"
 
-      } else if (boton.innerText == "Sacar") {
-        productosAcomprar = productosAcomprar.filter(el => el.id !== id) //Sacamos de la lista
-        
+      } else if (boton.innerText == "Sacar") {       
+        // Sacamos de la lista sin reasignar la variable
+        const index = productosAcomprar.findIndex(el => el.id === id);
+        if (index > -1) {
+          productosAcomprar.splice(index, 1); // Eliminamos el producto de la lista
+        }
+        console.log("eliminado id:" + id)
+        console.log(productosAcomprar)
+
         boton.style.backgroundColor = '';
         boton.innerText = "Agregar"
         titulo = "Eliminado"
       } 
-
-      console.log("se presiono boton")
-
-      console.log(productosAcomprar)
 
       Swal.fire({
         title: titulo,
@@ -114,8 +121,8 @@ const creadoraDeCards = (id, rubro, link, descripcion, agregado, comprado) => {
     });
     })
 
-
     Productos.append(container)
+    //document.querySelector('#product-container').appendChild(container);
 
-  }
+  };
 
