911f94f25c6e0ac87620f155e22de8e531fc6a36
yigoro <seba.bertolotti@gmail.com>
2025-01-09 16:45:09 -0300

    mejoras

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ let productosAcomprar = []  //se van a almnacenar los productos que se van a com
 Buscador.addEventListener("input", async (e) => {
   e.preventDefault();
 
-    //const response = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQyLVaEmCvBCmq3YVM7ysFHvbXNzcHAJY22K_-K7epbbe0Rso9uK3320s5twHwQwPAGMu2B5MLmPhys/pub?output=csv');
-    const response = await fetch('db-superApp.csv');
+    const response = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQyLVaEmCvBCmq3YVM7ysFHvbXNzcHAJY22K_-K7epbbe0Rso9uK3320s5twHwQwPAGMu2B5MLmPhys/pub?output=csv');
+    //const response = await fetch('db-superApp.csv');
     const csv = await response.text();
 
     const rows = csv.split("\n").slice(1); // Divide el CSV en filas y elimina la cabecera
@@ -32,8 +32,8 @@ Buscador.addEventListener("input", async (e) => {
 document.addEventListener("DOMContentLoaded", async ()=>{
 
   try {
-    const response = await fetch('public/db-superApp.csv');
-    //const response = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQyLVaEmCvBCmq3YVM7ysFHvbXNzcHAJY22K_-K7epbbe0Rso9uK3320s5twHwQwPAGMu2B5MLmPhys/pub?output=csv');
+    //const response = await fetch('public/db-superApp.csv');
+    const response = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQyLVaEmCvBCmq3YVM7ysFHvbXNzcHAJY22K_-K7epbbe0Rso9uK3320s5twHwQwPAGMu2B5MLmPhys/pub?output=csv');
     // Verificamos si la respuesta es exitosa
     if (!response.ok) {
       throw new Error('Error al obtener el CSV: ' + response.statusText);
@@ -125,4 +125,3 @@ const creadoraDeCards = (id, rubro, link, descripcion, productosAcomprar) => {
     //document.querySelector('#product-container').appendChild(container);
 
   };
-
